5e515f14e38cfac644dc89405a0fad3de7a01d69
Dilshad <dilshad.alam3215@gmail.com>
2020-12-02 17:38:19 +0545

    form-progress

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const showModal = document.querySelector('.show-modal');
 const displayModal = document.querySelector('.modal-container');
-const toFocus = document.querySelector('#to-focus');
+const websiteName = document.querySelector('#website-name');
+const websiteUrl = document.querySelector('#website-url');
+const formButton = document.querySelector('.form-button');
+const bookmarkForm = document.querySelector('.bookmark-form');
 const modalCross = document.querySelector('.modal-cross');
 
+//To show the modal after clicking add bookmarks
 
 function toShowModal(){
 console.log('working');
 displayModal.classList.add('display-modal');
-toFocus.focus();
+websiteName.focus();
 }
 
+// To remove modal from the screen
 function toRemoveClass(){
     displayModal.classList.remove('display-modal'); 
 }
+function removeClassOnOutsideClick(e){
+    e.target === displayModal ? displayModal.classList.remove('display-modal') : false;
+}
 
+//Form 
+function storeBookmark(e){
+    e.preventDefault();
+    const nameValue = websiteName.value;
+    let urlValue = websiteUrl.value;
+
+    if(!urlValue.includes('https://') && !urlValue.includes('http://')){
+        urlValue = `https://${urlValue}`;
+    }
+
+    console.log(nameValue);
+    console.log(urlValue);
+}
+
+
+//Modal Event listeners
 showModal.addEventListener('click',toShowModal);
 modalCross.addEventListener('click',toRemoveClass);
-window.addEventListener('click',(e)=>{
-    console.log(e.target)
-    e.target === displayModal ? displayModal.classList.remove('display-modal') : false;
-})
+window.addEventListener('click',removeClassOnOutsideClick)
+
+//Form Event listeners
+bookmarkForm.addEventListener('submit',storeBookmark);
